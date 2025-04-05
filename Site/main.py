a1cf37cee0f6248809a501fa1878445ea911e5b0
AhmetEren0064 <162623099+AhmetEren0064@users.noreply.github.com>
2025-04-05 10:58:32 +0300

    Add files via upload

--- Site/main.py
+++ Site/main.py
@@ -1,46 +1,49 @@
 from flask import Flask, render_template, request
 from advices import advices_list
 from random import choice
+import flask_sqlalchemy
 
 app = Flask(__name__)
+score = 0
+
+#* Veritabanı
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DBNAME.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = flask_sqlalchemy.SQLAlchemy(app)
+
+class UserScore(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(12), nullable=False)
+    user_password = db.Column(db.String(12), nullable=False)
+    user_score = db.Column(db.Integer)
+    
+    def __repr__(self):
+        return f'<UserScore {self.id}>'
 
 
-
-@app.route("/")#todo methods = ["POST"]
+#* Sayfalar
+@app.route("/", methods=["GET", "POST"])
 def adviceCards():
-    score = 0
+    global score
+    if request.method == "POST":
+        
+        if request.form.get("ive_did") == "clicked":
+            score += 10
+
     title = ""
     explaning = ""
-    time_list= ["1 hafta","2 hafta","3 hafta","1 ay","2 ay"]
-    time=choice(time_list)
+    time_list = ["1 hafta", "2 hafta", "3 hafta", "1 ay", "2 ay"]
+    time = choice(time_list)
     random_title = choice(list(advices_list.keys()))
     random_explaning = advices_list[random_title]
+
     return render_template('index.html',
-                                score=score,
-                                title=random_title,
-                                explaning=random_explaning,
-                                time=time
-                                )
-    
-""" if request.method == "POST":
-        if request.form.get("i_do_already") == "clicked" or request.form.get("another") == "clicked":
-            return render_template('index.html',
-                                score=score,
-                                title=random_title,
-                                explaning=random_explaning,
-                                time=time
-                                )
-            
-        elif request.form.get("ive-did") == "clicked":
-            score+=10
-            return render_template('index.html',
-                                score=score,
-                                title=random_title,
-                                explaning=random_explaning,
-                                time=time
-                                )"""
-        
-    
+                           score=score,
+                           title=random_title,
+                           explaning=random_explaning,
+                           time=time)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
